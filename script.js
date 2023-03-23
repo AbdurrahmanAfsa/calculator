1a8ff6b84916ea2ebe6545848b3f56a7c87afb88
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-23 10:39:04 +0200

    Make a function for each operation.

--- script.js
+++ script.js
@@ -57,3 +57,19 @@ zero.addEventListener('click' , () => {
 decimal.addEventListener('click' , () => {
     numberDisplay.textContent += ".";
 })
+
+function sum(number1 , number2){
+    return number1 + number2;
+}
+
+function subtract(number1 , number2){
+    return number1 - number2;
+}
+
+function multiply(number1 , number2){
+    return number1 * number2;
+}
+
+function divide(number1 , number2){
+    return number1 / number2;
+}
